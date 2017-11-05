5da1b001b2c5c14760ec63a0918efd325dd031b5
dvomaks <dvomaks@gmail.com>
2017-11-05 23:49:02 +0200

    Api methods

--- src/ClientContract.php
+++ src/ClientContract.php
@@ -3,5 +3,71 @@ namespace Dvomaks\Livecoin;
 
 interface ClientContract
 {
+
+
+    /**
+     * PUBLIC DATA
+     *
+     * Documentation https://www.livecoin.net/api/public
+     */
     public function exchangeTicker($params = []);
+    public function exchangeLastTrades($params);
+    public function exchangeOrderBook($params);
+    public function exchangeAllOrderBook($params);
+    public function exchangeMaxbidMinask($params);
+    public function exchangeRestrictions($params);
+    public function infoCoinInfo();
+
+
+    /**
+     * PRIVATE USER DATA
+     *
+     * Documentation https://www.livecoin.net/api/userdata
+     */
+    public function exchangeTrades($params = []);
+    public function exchangeClientOrders($params = []);
+    public function exchangeOrder($params);
+    public function paymentBalances($params = []);
+    public function paymentBalance($params);
+    public function paymentHistoryTransactions($params);
+    public function paymentHistorySize($params);
+    public function exchangeCommissions();
+    public function exchangeCommissionCommonInfo();
+
+
+    /**
+     * OPEN / CANCEL ORDERS
+     *
+     * Documentation https://www.livecoin.net/api/orders
+     */
+    public function exchangeBuylimit($params);
+    public function exchangeSelllimit($params);
+    public function exchangeBuymarket($params);
+    public function exchangeSellmarket($params);
+    public function exchangeCancellimit($params);
+
+
+    /**
+     * DEPOSIT AND WITHDRAWAL
+     *
+     * Documentation https://www.livecoin.net/api/withdrawal
+     */
+    public function paymentGetAddress($params);
+    public function paymentOutCoin($params);
+    public function paymentOutPayeer($params);
+    public function paymentOutCapitalist($params);
+    public function paymentOutAdvcah($params);
+    public function paymentOutCard($params);
+    public function paymentOutOkpay($params);
+    public function paymentOutPerfectmoney($params);
+
+
+    /**
+     * VOUCHERS
+     *
+     * Documentation https://www.livecoin.net/api/vouchers
+     */
+    public function paymentVoucherMake($params);
+    public function paymentVoucherAmount($params);
+    public function paymentVoucherRedeem($params);
 }
